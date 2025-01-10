73d62da2468aec08e36be539fb34b8d233922bcd
MHamed <aitsidiouali@gmail.com>
2025-01-10 13:31:43 +0100

    added the memory cards images and set them up in an object for access later

---
+++ src/asssets/images.jsx
@@ -0,0 +1,88 @@
+import Ace from './img/ace.jpg'
+import Eugene from  './img/eugen.jpg'
+import Frazer from './img/frazer.jpg'
+import Gohan from './img/gohan.jpg'
+import Goku from './img/Goku.jpeg'
+import Jinzouningen from './img/Jinzouningen.jpg'
+import KidMajinBoo from './img/kid-majin-boo.jpg'
+import Killua from './img/killua.jpg'
+import Mikasa from './img/mikasa.jpg'
+import Tanjiro from './img/tanjiro.jpg'
+import Vegeta from './img/vegeta.jpg'
+import Yoda from './img/Yoda.jpg'
+const Characters = [
+    {
+        id: 0,
+        img: Ace,
+        name: "Ace",
+        isClicked: false
+    },
+    {
+        id: 1,
+        img: Eugene,
+        name: "Eugene",
+        isClicked: false
+    },
+    {
+        id: 2,
+        img: Frazer,
+        name: "Frazer",
+        isClicked: false
+    },
+    {
+        id: 3,
+        img: Gohan,
+        name: "Gohan",
+        isClicked: false
+    },
+    {
+        id: 4,
+        img: Goku,
+        name: "Goku",
+        isClicked: false
+    },
+    {
+        id: 5,
+        img: Jinzouningen,
+        name: "Jinzouningen",
+        isClicked: false
+    },
+    {
+        id: 6,
+        img: KidMajinBoo,
+        name: "Kid majin boo",
+        isClicked: false
+    },
+    {
+        id: 7,
+        img: Killua,
+        name: "Killua",
+        isClicked: false
+    },
+    {
+        id: 8,
+        img: Mikasa,
+        name: "Mikasa",
+        isClicked: false
+    },
+    {
+        id: 9,
+        img: Tanjiro,
+        name: "Tanjiro",
+        isClicked: false
+    },
+    {
+        id: 10,
+        img: Vegeta,
+        name: "Vegeta",
+        isClicked: false
+    },
+    {
+        id: 11,
+        img: Yoda,
+        name: "Yoda",
+        isClicked: false
+    }
+]
+
+export default Characters;
